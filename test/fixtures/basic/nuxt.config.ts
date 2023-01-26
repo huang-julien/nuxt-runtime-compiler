c56d1371e8a739426180489301f0cf259854eb46
Julien Huang <63512348+huang-julien@users.noreply.github.com>
2023-01-26 22:36:21 +0100

    test: add test fixtures (#32)

---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import nuxtRuntimeCompiler from '../../../src/module'
+
+export default defineNuxtConfig({
+  modules: [
+    nuxtRuntimeCompiler
+  ],
+  experimental: {
+    externalVue: !process.env.NO_EXTERNAL_VUE
+  },
+  builder: process.env.BUILDER as 'vite' | 'webpack' ?? 'vite'
+})
