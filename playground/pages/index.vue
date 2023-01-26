916bb073c48fe92747fecc47ff46d71ee499b457
julien huang <julien.huang@outlook.fr>
2023-01-26 22:46:03 +0100

    chore(playground): lint and fix types

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Component } from 'vue'
 import Helloworld from '../components/Helloworld.vue'
 const ready = useState('ready', () => false)
 
@@ -18,12 +19,12 @@ const compTemplate = computed(() => `
  */
 const ComponentDefinedInSetup = computed(() => h({
   template: compTemplate.value
-}))
+}) as Component)
 
 /**
  * block the navigation until api calls finishes
  */
-const { data } = await useAsyncData('templates', async () => {
+const { data, pending } = await useAsyncData('templates', async () => {
   const [interactiveComponent, templateString] = await Promise.all([
     $fetch('/api/full-component'),
     $fetch('/api/template')
@@ -37,12 +38,16 @@ const { data } = await useAsyncData('templates', async () => {
   }
 }, {})
 
+if (!data.value) {
+  throw createError(':( something went wrong')
+}
+
 /**
  * you can even auto generate a list of component in a composable
  * then provide it to all children using provide()/inject() !
  */
 const Interactive = h({
-  template: data.value.interactiveComponent.template,
+  template: data.value?.interactiveComponent.template,
   setup (props) {
     /**
      * use new Function for closure
@@ -53,16 +58,16 @@ const Interactive = h({
       'ref',
       'computed',
       'props',
-      data.value.interactiveComponent.setup
+      data.value?.interactiveComponent.setup ?? ''
     )(ref, computed, props)
   },
-  props: data.value.interactiveComponent.props
-})
+  props: data.value?.interactiveComponent.props
+}) as Component
 </script>
 
 <template>
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
-  <div>
+  <div v-if="!pending">
     <Helloworld />
     <ComponentDefinedInSetup />
     <button @click="count++">
@@ -70,7 +75,7 @@ const Interactive = h({
     </button>
     <template v-if="ready">
       <Name template="<div>I'm the Name.ts component</div>" />
-      <show-template :template="data.templateString" name="Julien" />
+      <show-template :template="data?.templateString" name="Julien" />
       <Interactive lastname="Huang" firstname="Julien" />
     </template>
   </div>
